fcdabc1710a96894537d6da9f65b52b3ef238334
Miguel <Miguel@MIGUEL-PC>
2020-11-07 20:07:43 -0600

    Agregar archivos de proyecto.

---
+++ EJERCICIO-02/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIO_02
+{
+    class Program
+    {
+        public static decimal[] salarioActual = new decimal[5];
+        public static decimal[] salarioAnterior = new decimal[5];
+        public static decimal[] sumaTotal = new decimal[salarioActual.Length];
+        static decimal totalDosArray = 0;
+        static decimal sumaSalarioactual = 0;
+        static decimal sumaSalarioAnterior = 0;
+        static decimal promedioSalaAnterior = 0;
+        static decimal promedioSalactual = 0;
+        static decimal promedioTotal = 0;
+
+        static void Main(string[] args)
+        {
+
+            sueldosActuales();
+            sumatotalSalarioactual();
+            salarioPromedio();
+            sueldosAnetrior();
+            sumaTotalsalAnterior();
+            mostrarTotal();
+          
+           // Console.WriteLine("Total de salario actual: {0}", sumaSalarioactual);
+            Console.ReadKey();
+        }
+        //ingresar los 20 salarios actuales
+        public static decimal[] sueldosActuales()
+        {
+            Console.WriteLine("Ingrese salario actual : \n");
+
+            for (int i = 0; i < salarioActual.Length; i++)
+            {
+                Console.WriteLine("Salario {0} ",i + 1);
+                salarioActual[i] = decimal.Parse(Console.ReadLine());
+            }
+
+            return salarioActual;
+        }
+        //suma de salario actuales
+        public static void sumatotalSalarioactual()
+        {
+           for(int i=0; i<salarioActual.Length; i++)
+            {
+                sumaSalarioactual += salarioActual[i];
+            } 
+        }
+        //salario promedio
+        private static void salarioPromedio()
+        {
+            promedioSalactual = sumaSalarioactual / salarioActual.Length;
+            promedioSalaAnterior = sumaSalarioAnterior / salarioAnterior.Length;
+            promedioTotal = totalDosArray / salarioActual.Length;
+        }
+        //sueldo anterior
+        public static decimal[] sueldosAnetrior()
+        {
+            Console.WriteLine("Ingrese salario anterior\n");
+
+            for (int i = 0; i < salarioAnterior.Length; i++)
+            {
+                Console.WriteLine("Salario {0}:",i + 1);
+                salarioAnterior[i] = decimal.Parse(Console.ReadLine());
+            }
+            return salarioActual;
+
+        }
+        //suma salarios anteriores
+        public static void sumaTotalsalAnterior()
+        {
+            for (int i = 0; i < salarioAnterior.Length; i++)
+            {
+                sumaSalarioAnterior += salarioAnterior[i];
+            }
+        }
+        //Método para mostrar los salarios
+        public static void mostrarTotal()
+        {
+            Console.WriteLine("\nSALARIO ANTERIOR\t\t SALARIO ACTUAL \t\tTOTAL");
+
+            for (int i = 0; i < salarioActual.Length; i++)
+            {
+                sumaTotal[i] = salarioActual [i] + salarioAnterior[i];
+                totalDosArray += sumaTotal[i];
+                Console.WriteLine(" {0}\t\t\t\t {1} \t\t\t\t {2} ", salarioAnterior[i], salarioActual[i], sumaTotal[i]);
+            }
+            Console.WriteLine("\ntotal: {0}\t\t\t {1}\t\t\t\t {2}", sumaSalarioAnterior, sumaSalarioactual, totalDosArray);
+
+            Console.WriteLine("\nPromedios: {0}\t\t\t {1} \t\t\t{2}", promedioSalaAnterior, promedioSalactual, promedioTotal);
+        }
+
+    }
+}
---
+++ GUIA1_23082020PROGRA2/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace GUIA1_23082020PROGRA2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* ALUMNO: KEVIN MIGUEL HENRIQUEZ PEREZ
+             * CARRERA: LIC. EN COMPUTACIÓN
+             * ASIGNATURA: PROGRAMACION II
+             * CICLO 02-2020
+             * GRUPO DE CLASE: 1
+             * GUIA 1
+             */
+            expresionMate();
+            expresionNumeroDos();
+
+            Console.ReadKey();
+        }
+
+        //EJERCICIO 01
+        public static void expresionMate()
+        {
+            Console.WriteLine("EJERCICIO 1.1 METODO 1\n\n");
+            Console.WriteLine("ingrese la expresión");
+            string expresion = Console.ReadLine().Trim(); //lee la expresion 
+            int apertura = 0;
+            int cierre = 0;
+         
+            for (int i = 0; i < expresion.Length; i++)
+            {
+                if (expresion[i] == '(') //evalua una expresion si solo hay una apertura pero sin cierre
+                {
+                    apertura++;
+                }
+          
+                if (expresion[i] == ')')//evalua si tiene cierre y no apertura
+                {
+                    cierre++;
+                }
+            }
+            if (apertura == cierre) //evalua si los parententesis de apertura es igual a los de cierre. en la expresion dada
+            {
+                Console.WriteLine("Expresion correcta");
+            }
+            else
+            {
+                Console.WriteLine("ERROR: Expresion no valida");
+            }
+        }
+
+        //EJERCICIO 02
+        public static void expresionNumeroDos()
+        {
+            Console.WriteLine("\n\nEJERCICIO 1.2 METODO 2\n\n");
+            Console.WriteLine("ingrese la expresión");
+            string expresion = Console.ReadLine().Trim(); //lee la expresion 
+            int apertura = 0;
+            int cierre = 0;
+            bool operacionBasic = true;
+            bool operacionBasicDos = true;
+             
+            for (int i = 0; i < expresion.Length; i++)
+            {
+                if (expresion[i] == '(') //evalua una expresion si solo hay una apertura pero sin cierre
+                {
+                    apertura++;
+                    operacionBasic = false;
+                }
+                if (expresion[i] - 1 == '+' + ')' || expresion[i] - 1 == '-' + ')' || expresion[i] - 1 == '*' + ')' || expresion[i] - 1 == '/' + ')' || expresion[i] - 1 == '^' + ')')
+                {
+                    
+                    operacionBasic = true;
+                    
+                }
+                if (expresion[i] == ')')//evalua si tiene cierre y no apertura
+                {
+                    cierre++;
+                    operacionBasicDos = false;
+                }
+                if (expresion[i] - 1 == '(' + '+' || expresion[i] - 1 == '(' + '-' || expresion[i] - 1 == '(' + '-' || expresion[i] - 1 == '(' + '/' || expresion[i] - 1 == '(' + '^')
+                {
+                    operacionBasicDos = true;
+                }
+
+            }
+
+            if ((apertura == cierre) && operacionBasic == false) //evalua si los parententesis de apertura es igual a los de cierre. en la expresion dada
+            {
+                Console.WriteLine("Expresion correcta");
+                // return  resultado = true;
+            }
+            else
+            {
+                Console.WriteLine("ERROR: Expresion no valida");
+            }
+ 
+            if(operacionBasic || operacionBasicDos)
+            {
+                Console.WriteLine("ERROR: Hay un operador antes o despues del parentesis");
+            }
+            
+        }
+    }
+}
